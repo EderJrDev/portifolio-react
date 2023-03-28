7fc4c673f7a5088cedae6b4f4ac3628274103197
yatocham <ederjuninho2003@gmail.com>
2023-03-28 20:43:46 -0300

    Responsive

--- src/components/tech.jsx
+++ src/components/tech.jsx
@@ -4,10 +4,7 @@ import '@splidejs/react-splide/css'; import '@splidejs/react-splide/css/skyblue'
 import '@splidejs/react-splide/css/sea-green';
 import '@splidejs/react-splide/css/core';
 import { Splide, SplideTrack, SplideSlide } from '@splidejs/react-splide';
-
-// import { Splide } from '@splidejs/splide';
 import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
-import tech from '../assets/img/bg-1.jpg';
 
 export function Tech() {
     return (
@@ -21,59 +18,55 @@ export function Tech() {
                             Tecnologias que já tive contato, utilizei em projetos e nos esturdos.
                         </p>
                     </article>
-                    {/* <Splide aria-label="My Favorite Images">
-                        <SplideSlide>
-                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="Image 1" />
-                        </SplideSlide>
-                        <SplideSlide>
-                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="Image 2" />
-                        </SplideSlide>
-                    </Splide> */}
-                    <Splide hasTrack={false} aria-label="..." options={{
-                        rewind: true,
-                        autoStart: true,
-                        // width: 800,
-                        gap: '1rem',
-                        autoScroll: {
-                            speed: 2,
-                        },
-                    }}>
-                        <div className="custom-wrapper text-center">
+                    <div>
+                        <Splide hasTrack={false} aria-label="..." options={{
+                            type: "loop",
+                            gap: "10px",
+                            drag: "free",
+                            arrows: false,
+                            pagination: false,
+                            perPage: 3,
+                            autoScroll: {
+                                pauseOnHover: false,
+                                pauseOnFocus: false,
+                                rewind: false,
+                                speed: 1
+                            }
+                        }}
+                            extensions={{ AutoScroll }}
+                        >
                             <SplideTrack>
                                 <SplideSlide>
-                                    <img width={300} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react" />
+                                    <img className="img-tech" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="react" />
                                 </SplideSlide>
                                 <SplideSlide>
-                                    <img width={300} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" alt="javascript" />
+                                    <img className="img-tech" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg" alt="javascript" />
                                 </SplideSlide>
                                 <SplideSlide>
-                                    <img width={300} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="typescript" />
+                                    <img className="img-tech" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="typescript" />
                                 </SplideSlide>
                                 <SplideSlide>
-                                    <img width={300} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" alt="css3" />
+                                    <img className="img-tech" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" alt="css3" />
                                 </SplideSlide>
                                 <SplideSlide>
-                                    <img width={300} src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" alt="html5" />
+                                    <img className="img-tech" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" alt="html5" />
                                 </SplideSlide>
                                 <SplideSlide>
-                                    <img width={300} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="python" />
+                                    <img className="img-tech" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="python" />
                                 </SplideSlide>
                                 <SplideSlide>
-                                    <img width={300} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" alt="php" />
+                                    <img className="img-tech" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg" alt="php" />
                                 </SplideSlide>
                                 <SplideSlide>
-                                    <img width={300} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="github" />
+                                    <img className="img-tech" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="github" />
                                 </SplideSlide>
                                 <SplideSlide>
-                                    <img width={300} src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="git" />
+                                    <img className="img-tech" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="git" />
                                 </SplideSlide>
-
                             </SplideTrack>
-                            <div className="splide__arrows" />
-                        </div>
-                    </Splide>
-
-                    <div class="section-div text-center">
+                        </Splide>
+                    </div>
+                    {/* <div class="section-div text-center">
                         <div>
                             <ul class="channel-list">
                                 <li>
@@ -82,7 +75,7 @@ export function Tech() {
                                             alt="react" />
                                     </a>
                                 </li>
-                               
+
                                 <li>
                                     <a target="_blank" href="#">
                                         <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"
@@ -121,7 +114,7 @@ export function Tech() {
                                 </li>
                             </ul>
                         </div>
-                    </div>
+                    </div> */}
                 </Col>
             </Row>
             <Container />
